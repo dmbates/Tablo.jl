96a0e828e4f2755a67d8430aa386d06b1b3b330b
Douglas Bates <dmbates@gmail.com>
2015-12-01 13:10:18 -0600

    Add a simple function to tabulate series episodes.

--- src/channels.jl
+++ src/channels.jl
@@ -2,3 +2,17 @@ chanids() = json(get(ch_ids))["ids"]
 chinfo(id) = json(get(string(ch_info,id)))
 recids() = json(get(rec_ids))["ids"]
 recinfo(id) = json(get(string(rec_info,id)))
+
+function episodetable(ids::Vector)
+    ans = Dict{AbstractString,Int}()
+    for id in ids
+        try
+            info = recinfo(id)
+            tt = string(info["meta"]["recSeries"]["jsonForClient"]["title"])
+            ans[tt] = get(ans,tt,0) + 1
+        catch
+            next
+        end
+    end
+    ans
+end
